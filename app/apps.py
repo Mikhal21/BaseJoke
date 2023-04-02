04abcb4189303ebd0521323dce0389c1d973f26a
Mikhal21 <mikhal.ifergan@dauphine.eu>
2023-04-02 11:34:53 +0200

    "The route returns a JSON representation of a quote for a given quote id"

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
 
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/quote/<int:quote_id>')
+def quote(quote_id):
+    with open('quotes.json') as f:
+        quotes = json.load(f)
+    for quote in quotes:
+        if quote['id'] == quote_id:
+            return jsonify(quote)
+    return jsonify({'error': 'Quote not found'})
+
+
 
 class MainAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
+
